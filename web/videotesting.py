7a8737b7960c8c15905319e9e6a2d06dc9187fa1
mruchus <mruchus@icloud.com>
2022-11-24 21:01:21 +0000

    update gitignore

--- web/videotesting.py
+++
@@ -1,9 +0,0 @@
-import gradio as gr
-# dont cry if the executable is not found. just download it bestie
-
-def video_identity(video):
-    return video
-# (type?, input, output)
-webvideo = gr.Interface(video_identity, inputs=gr.Video(), outputs=gr.Video())
-
-webvideo.launch(share=True)
